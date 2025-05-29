4594ab476807c603657753fd59ff9b41ee866266
PedroMarquez32 <pedrojaviermarquelizana@gmail.com>
2025-05-29 21:24:43 +0200

    fix 43

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://proyectofinalfrontend-production-65e3.up.railway.app' || "https://traveldreams.com",
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || "https://proyectofinal-production-b22c.up.railway.app" || "https://traveldreams.com",
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true
 }));
