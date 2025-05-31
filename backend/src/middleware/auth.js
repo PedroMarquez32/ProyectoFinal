29db019359ff5f8cb7a0eef643c60168c02fa40c
PedroMarquez32 <pedrojaviermarquelizana@gmail.com>
2025-05-31 22:17:41 +0200

    arreglo auth deploy

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+const sequelize = require('../config/database');
+const User = require('../models/User')(sequelize);
 
 const auth = async (req, res, next) => {
   try {
