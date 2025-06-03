1207a5065a7c130e53f099c913f4770f5fd88a46
PedroMarquez32 <pedrojaviermarquelizana@gmail.com>
2025-06-03 20:48:33 +0200

    arreglos toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { toast } from 'react-toastify'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <ToastContainer position="top-center" autoClose={3000} />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
